bc7ea7afc12f00429fe9957cb987d17a95de97a3
Tamás Benke <tombenke@gmail.com>
2019-01-06 16:22:06 +0100

    Add LICENSE.md

--- .kickoff.sh
+++ .kickoff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export files="package.json package-lock.json README.md"
+export files="package.json package-lock.json README.md LICENSE.md"
 for f in $files; do
     sed -f .kickoff.sed -i $f
 done
